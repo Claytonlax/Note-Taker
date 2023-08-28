4132912c56ca1326b96463b145b902aeefafd5aa
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-08-27 18:38:34 -0700

    fixed route, added code for new notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,31 @@
 const app = require('express').Router();
-const allNotes = require("../db/db.json")
-const {readFromFile, writeToFile, readAndAppend} = require('../helpers/fsUtils')
+const {readFromFile, readAndAppend} = require('../helpers/fsUtils')
+const uuid = require('../helpers/uuid')
 
 app.get('/notes', (req,res)=> {
 console.log('in get /api/notes')
+ readFromFile('../../../../../../Users/clayt/bootcamp/WORKING/Note-Taker/db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 app.post('/notes', (req,res)=>{
     console.log('in post /api/notes')
 
-});
+    const { title, text } = req.body;
+
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      readAndAppend(newNote, '../db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
+
 
 
 module.exports = app
